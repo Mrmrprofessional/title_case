17788c2cbe2fbb1220a93425f0b2896cf5b23a29
mike rapp & filipe silvestre <student@epicodus.com>
2015-08-10 14:49:09 -0700

    add regex

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -3,10 +3,11 @@
     {
         function makeTitleCase($input_title)
         {
-            $exception_list = array('of','a','the','and','an','or','nor','but','is','if','then','else','when',
-'at','from','by','on','off','for','in','out','over','to','into','with');
+            $exception_list = array('of','a','the','and','an','or','nor','but','is','if','then','else','when','at','from','by','on','off','for','in','out','over','to','into','with');
+            $word_splitters = array(' ', '-', "O'", "L'", "D'", 'St.', 'Mc');
+            $regex = "/\s/";
 
-            $input_array_of_words = explode(" ", strtolower($input_title));
+            $input_array_of_words = preg_split($regex, strtolower($input_title), -1, PREG_SPLIT_DELIM_CAPTURE);
             foreach ($input_array_of_words as $key => $word)
             {
                 if (!in_array($word, $exception_list) or $word == $input_array_of_words[0])
