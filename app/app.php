450646e49a276517ddd52ecb8ffe537adb5754b5
mike rapp & filipe silvestre <student@epicodus.com>
2015-08-10 15:18:13 -0700

    add entry form

--- app/app.php
+++ app/app.php
@@ -2,7 +2,7 @@
 
     // DEPENDENCIES
         require_once __DIR__."/../vendor/autoload.php"; // frameworks
-        require_once __DIR__."/../src/First.php"; // example of filepath to first Object created
+        require_once __DIR__."/../src/TitleCaseGenerator.php"; // example of filepath to first Object created
 
     // INITIALIZE COOKIE SESSION
 
@@ -16,9 +16,15 @@
         // display index webpage
         $app->get('/', function() use ($app) {
 
-            return $app['twig']->render('index.html.twig');
+            return $app['twig']->render('form.html.twig');
         });
 
+        $app->get("/view_title_case", function() use($app)
+        {
+            $my_TitleCaseGenerator = new TitleCaseGenerator;
+            $title_cased_phrase = $my_TitleCaseGenerator->makeTitleCase($_GET['phrase']);
+            return $app['twig']->render('title_cased.html.twig', array('result' => $title_cased_phrase));
+        });
 
     return $app;
 
